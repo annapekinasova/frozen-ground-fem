470795c390c5317620a40ef778dce0fad0b8a5cf
Brandon Karchewski <brandon.karchewski@ucalgary.ca>
2023-01-22 16:19:27 -0700

    fixed PEP-8 issues with flake8

--- examples/simple_therm.py
+++ examples/simple_therm.py
@@ -3,27 +3,24 @@ from frozen_ground_fem.geometry import Point1D
 
 def main():
     print("successfully imported frozen_ground_fem")
-    
+
     p = Point1D(3)
     print(f"created a Point1D with ID {id(p)}")
     print(f"coords: {p.coords}")
     print(f"z: {p.z}")
     p.z = 2
     print(f"z: {p.z}")
-    
+
     point_list = [Point1D(k) for k in range(10)]
     for pp in point_list:
         print(pp.z)
-    
+
     print("trying p = Point1D('three')")
     try:
         p = Point1D("three")
     except ValueError:
         print("raised ValueError as expected")
-    except:
-        raise
 
 
 if __name__ == "__main__":
     main()
-
--- src/frozen_ground_fem/geometry.py
+++ src/frozen_ground_fem/geometry.py
@@ -14,10 +14,10 @@ class Point1D:
     @property
     def z(self):
         return self.coords[0]
-        
+
     @z.setter
     def z(self, value):
         self.coords[0] = value
-        
+
     def __str__(self):
         return self.coords.__str__()
