b93703e870958d6d7a0ac37359a1fb77f50f106b
Anna Pekinasova <anna.pekinasova@ucalgary.ca>
2023-01-23 09:38:57 -0700

    implemented Node1D class

--- examples/simple_therm.py
+++ examples/simple_therm.py
@@ -1,4 +1,7 @@
-from frozen_ground_fem.geometry import Point1D
+from frozen_ground_fem.geometry import (
+    Point1D,
+    Node1D,
+    )
 
 
 def main():
@@ -10,6 +13,10 @@ def main():
     print(f"z: {p.z}")
     p.z = 2
     print(f"z: {p.z}")
+    print(f"type(p) is Node1D: {type(p) is Node1D}")
+    print(f"type(p) is Point1D: {type(p) is Point1D}")
+    print(f"isinstance(p, Node1D): {isinstance(p, Node1D)}")
+    print(f"isinstance(p, Point1D): {isinstance(p, Point1D)}")
 
     point_list = [Point1D(k) for k in range(10)]
     for pp in point_list:
@@ -21,6 +28,25 @@ def main():
     except ValueError:
         print("raised ValueError as expected")
 
+    nd = Node1D(0.5, -5)
+    print(f"created a Node1D with ID {id(nd)}")
+    print(f"z: {nd.z}, temp: {nd.temp}")
+    nd.temp = -2
+    print(f"z: {nd.z}, temp: {nd.temp}")
+    print(f"type(nd) is Node1D: {type(nd) is Node1D}")
+    print(f"type(nd) is Point1D: {type(nd) is Point1D}")
+    print(f"isinstance(nd, Node1D): {isinstance(nd, Node1D)}")
+    print(f"isinstance(nd, Point1D): {isinstance(nd, Point1D)}")
+
+    print("trying nd.temp = 'three'")
+    try:
+        nd.temp = 'three'
+    except ValueError:
+        print("raised ValueError as expected")
+        
+    print(f"str(nd): {str(nd)}")
+    print(nd)
+    
 
 if __name__ == "__main__":
     main()
--- src/frozen_ground_fem/geometry.py
+++ src/frozen_ground_fem/geometry.py
@@ -21,3 +21,22 @@ class Point1D:
 
     def __str__(self):
         return self.coords.__str__()
+        
+        
+class Node1D(Point1D):
+
+    def __init__(self, coord=0., temp=0.):
+        super().__init__(coord)
+        self._temp = np.zeros((1,))
+        self.temp = temp
+
+    @property
+    def temp(self):
+        return self._temp[0]
+
+    @temp.setter
+    def temp(self, value):
+        self._temp[0] = value
+
+    def __str__(self):
+        return super().__str__() + f", temp={self.temp}"
