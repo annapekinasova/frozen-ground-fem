c7b7cf5de42a1705e8fc204d929dbf42c272eeb7
Anna Pekinasova <anna.pekinasova@ucalgary.ca>
2023-04-18 16:58:12 -0600

    added docstrings to ThermalElement1D

--- src/frozen_ground_fem/thermal.py
+++ src/frozen_ground_fem/thermal.py
@@ -1,3 +1,8 @@
+"""thermal.py
+Module for implementing thermal physics using the finite element method.
+"""
+import numpy as np
+
 from frozen_ground_fem.geometry import (
     shape_matrix,
     gradient_matrix,
@@ -6,6 +11,21 @@ from frozen_ground_fem.geometry import (
 
 
 class ThermalElement1D(Element1D):
+    """Class for computing element matrices for thermal physics.
+
+    Attributes
+    ----------
+    nodes
+    int_pts
+    jacobian
+
+    Raises
+    ------
+    TypeError
+        If parent initializer is not a
+        :c:`frozen_ground_fem.geometry.Element1D`.
+    """
+
     def __init__(self, parent: Element1D):
         if not isinstance(parent, Element1D):
             raise TypeError(f"type(parent): {type(parent)} is not Element1D")
@@ -13,17 +33,61 @@ class ThermalElement1D(Element1D):
 
     @property
     def nodes(self):
+        """The tuple of :c:`Node1D` contained in the element.
+
+        Returns
+        ------
+        tuple of :c:`Node1D`
+
+        Notes
+        -----
+        This is a wrapper that references the nodes property
+        of the parent Element1D.
+        """
         return self._parent.nodes
 
     @property
     def jacobian(self):
+        """The length scale of the element (in Lagrangian coordinates).
+
+        Returns
+        -------
+        float
+
+        Notes
+        -----
+        This is a wrapper that references the jacobian property
+        of the parent Element1D.
+        """
         return self._parent.jacobian
 
     @property
     def int_pts(self):
+        """The tuple of :c:`IntegrationPoint1D` contained in the element.
+
+        Returns
+        ------
+        tuple of :c:`IntegrationPoint1D`
+
+        Notes
+        -----
+        This is a wrapper that references the int_pts property
+        of the parent Element1D.
+        """
         return self._parent.int_pts
 
     def heat_flow_matrix(self):
+        """The element heat flow (conduction) matrix.
+
+        Returns
+        -------
+        numpy.ndarray, shape=(2, 2)
+
+        Notes
+        -----
+        Integrates B^T * lambda * B over the element
+        where lambda is the thermal conductivity.
+        """
         B = gradient_matrix(0, 1)
         H = np.zeros_like(B.T @ B)
         jac = self.jacobian
@@ -34,6 +98,17 @@ class ThermalElement1D(Element1D):
         return H
 
     def heat_storage_matrix(self):
+        """The element heat storage matrix.
+
+        Returns
+        -------
+        numpy.ndarray, shape=(2, 2)
+
+        Notes
+        -----
+        Integrates N^T * C * N over the element
+        where C is the volumetric heat capacity.
+        """
         N = shape_matrix(0)
         C = np.zeros_like(N.T @ N)
         jac = self.jacobian
