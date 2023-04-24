8f7f25a552d028d6527919d610c23b0d6f08a7e2
Brandon Karchewski <brandon.karchewski@ucalgary.ca>
2023-04-24 10:14:25 -0600

    created setUp for TestThermalElement1D

---
+++ tests/test_thermal.py
@@ -0,0 +1,18 @@
+import unittest
+
+import numpy as np
+
+from frozen_ground_fem.geometry import (
+    Node1D,
+    Element1D,
+)
+from frozen_ground_fem.thermal import (
+    ThermalElement1D,
+)
+
+
+class TestThermalElement1D(unittest.TestCase):
+    def setUp(self):
+        self.nodes = tuple(Node1D(k, 2.0 * k + 1.0) for k in range(2))
+        self.e = Element1D(self.nodes)
+        self.thrm_e = ThermalElement1D(self.e)
