d598dad2e4478531c8b22b7ba52ddd2dd1d1fdb6
Anna Pekinasova <anna.pekinasova@ucalgary.ca>
2023-04-26 09:56:20 -0600

    initiated tests for ThermalBoundary1D

--- tests/test_thermal.py
+++ tests/test_thermal.py
@@ -11,6 +11,7 @@ from frozen_ground_fem.geometry import (
 )
 from frozen_ground_fem.thermal import (
     ThermalElement1D,
+    ThermalBoundary1D,
 )
 
 
@@ -40,7 +41,8 @@ class TestThermalElement1D(unittest.TestCase):
             self.assertIs(e_ip, te_ip)
 
     def test_heat_flow_matrix_uninitialized(self):
-        self.assertTrue(np.allclose(self.thrm_e.heat_flow_matrix(), np.zeros((2, 2))))
+        self.assertTrue(np.allclose(
+            self.thrm_e.heat_flow_matrix(), np.zeros((2, 2))))
 
     def test_heat_flow_matrix(self):
         m = Material(thrm_cond_solids=1e-5)
@@ -67,4 +69,40 @@ class TestThermalElement1D(unittest.TestCase):
         heat_cap = self.thrm_e.int_pts[0].vol_heat_cap
         jac = self.thrm_e.jacobian
         expected = heat_cap * jac / 6 * np.array([[2.0, 1.0], [1.0, 2.0]])
-        self.assertTrue(np.allclose(self.thrm_e.heat_storage_matrix(), expected))
+        self.assertTrue(np.allclose(
+            self.thrm_e.heat_storage_matrix(), expected))
+
+
+class TestThermalBoundary1D(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_invalid_no_parent(self):
+        pass
+
+    def test_invalid_parent(self):
+        pass
+
+    def test_defaults(self):
+        pass
+
+    def test_nodes_equal(self):
+        pass
+
+    def test_assign_bnd_type_invalid(self):
+        pass
+
+    def test_assign_bnd_type_valid(self):
+        pass
+
+    def test_assign_bnd_value_invalid(self):
+        pass
+
+    def test_assign_bnd_value_valid(self):
+        pass
+
+    def test_update_nodes_bnd_type_temp(self):
+        pass
+
+    def test_update_nodes_bnd_type_non_temp(self):
+        pass
