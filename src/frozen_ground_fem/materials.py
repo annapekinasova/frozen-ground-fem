2112403bba8e9f0a60f996f6f789a4285ae5802b
Brandon Karchewski <brandon.karchewski@ucalgary.ca>
2023-05-22 13:21:10 -0600

    reviewed deg_sat_water and hyd_cond parameters

--- src/frozen_ground_fem/materials.py
+++ src/frozen_ground_fem/materials.py
@@ -302,8 +302,7 @@ class Material:
 
     @property
     def hyd_cond_index(self):
-        """Hydraulic conductivity index constant of unfrozen soil []
-        for calculation of void ratio difference.
+        """Hydraulic conductivity index constant of unfrozen soil.
 
         Parameters
         ----------
