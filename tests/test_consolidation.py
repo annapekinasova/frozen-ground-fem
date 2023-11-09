b48182459b027e9269c55bab8c502476862d7e5e
Anna Pekinasova <anna.pekinasova@ucalgary.ca>
2023-11-09 09:11:34 -0700

    initiated tests for consolidation and thermal for order 1 and 3

--- tests/test_consolidation.py
+++ tests/test_consolidation.py
@@ -31,21 +31,22 @@ class TestConsolidationElement1D(unittest.TestCase):
             ConsolidationElement1D(self.nodes)
 
     def test_jacobian_equal(self):
-        self.assertAlmostEqual(self.e.jacobian, self.thrm_e.jacobian)
+        self.assertAlmostEqual(self.e.jacobian, self.cond_e.jacobian)
 
     def test_nodes_equal(self):
-        for nd, e_nd in zip(self.nodes, self.thrm_e.nodes):
+        for nd, e_nd in zip(self.nodes, self.cond_e.nodes):
             self.assertIs(nd, e_nd)
-
+            
+############# order=1 and order=3 tests
     def test_int_pts_equal(self):
-        for e_ip, te_ip in zip(self.e.int_pts, self.thrm_e.int_pts):
+        for e_ip, te_ip in zip(self.e.int_pts, self.cond_e.int_pts):
             self.assertIs(e_ip, te_ip)
-
-    def test_heat_flow_matrix_uninitialized(self):
+            
+    def test_stiffness_matrix_uninitialized(self):
         self.assertTrue(np.allclose(
-            self.thrm_e.heat_flow_matrix, np.zeros((2, 2))))
+            self.cond_e.stiffness_matrix, np.zeros((2, 2))))
 
-    def test_heat_flow_matrix(self):
+    def test_stiffness_matrix(self):
         m = Material(thrm_cond_solids=1e-5)
         for ip in self.thrm_e.int_pts:
             ip.material = m
