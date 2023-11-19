ca6c5e6ff5d36534fa877f4cf9feb8a138aba464
Brandon Karchewski <brandon.karchewski@ucalgary.ca>
2023-11-19 14:45:43 -0700

    added cubic test cases for thermal element

--- tests/test_thermal.py
+++ tests/test_thermal.py
@@ -7,6 +7,7 @@ from frozen_ground_fem.materials import (
 )
 from frozen_ground_fem.geometry import (
     Node1D,
+    IntegrationPoint1D,
     Element1D,
     Boundary1D,
 )
@@ -16,19 +17,24 @@ from frozen_ground_fem.thermal import (
 )
 
 
-class TestThermalElement1D(unittest.TestCase):
+class TestThermalElement1DInvalid(unittest.TestCase):
     def setUp(self):
         self.nodes = tuple(Node1D(k, 2.0 * k + 1.0) for k in range(2))
-        self.e = Element1D(self.nodes, order=1)
-        self.thrm_e = ThermalElement1D(self.e)
 
-    def test_invalid_no_parent(self):
+    def test_no_parent(self):
         with self.assertRaises(TypeError):
             ThermalElement1D()
 
     def test_invalid_parent(self):
         with self.assertRaises(TypeError):
             ThermalElement1D(self.nodes)
+
+
+class TestThermalElement1DLinear(unittest.TestCase):
+    def setUp(self):
+        self.nodes = tuple(Node1D(k, 2.0 * k + 1.0) for k in range(2))
+        self.e = Element1D(self.nodes, order=1)
+        self.thrm_e = ThermalElement1D(self.e)
 
     def test_jacobian_equal(self):
         self.assertAlmostEqual(self.e.jacobian, self.thrm_e.jacobian)
@@ -40,15 +46,10 @@ class TestThermalElement1D(unittest.TestCase):
     def test_int_pts_equal(self):
         for e_ip, te_ip in zip(self.e.int_pts, self.thrm_e.int_pts):
             self.assertIs(e_ip, te_ip)
-            
-############# order=1 and order=3 tests
-    def test_heat_flow_matrix_uninitialized_linear(self):
+
+    def test_heat_flow_matrix_uninitialized(self):
         self.assertTrue(np.allclose(
             self.thrm_e.heat_flow_matrix, np.zeros((2, 2))))
-    
-    def test_heat_flow_matrix_uninitialized_cubic(self):
-        self.assertTrue(np.allclose(
-            self.cond_e.stiffness_matrix, np.zeros((4, 4))))
 
     def test_heat_flow_matrix(self):
         m = Material(thrm_cond_solids=1e-5)
@@ -79,10 +80,75 @@ class TestThermalElement1D(unittest.TestCase):
             self.thrm_e.heat_storage_matrix, expected))
 
 
+class TestThermalElement1DCubic(unittest.TestCase):
+    def setUp(self):
+        self.nodes = tuple(Node1D(k, 2.0 * k + 1.0) for k in range(4))
+        self.e = Element1D(self.nodes, order=3)
+        self.thrm_e = ThermalElement1D(self.e)
+
+    def test_jacobian_equal(self):
+        self.assertAlmostEqual(self.e.jacobian, self.thrm_e.jacobian)
+
+    def test_nodes_equal(self):
+        for nd, e_nd in zip(self.nodes, self.thrm_e.nodes):
+            self.assertIs(nd, e_nd)
+
+    def test_int_pts_equal(self):
+        for e_ip, te_ip in zip(self.e.int_pts, self.thrm_e.int_pts):
+            self.assertIs(e_ip, te_ip)
+
+    def test_heat_flow_matrix_uninitialized(self):
+        self.assertTrue(np.allclose(
+            self.thrm_e.heat_flow_matrix, np.zeros((4, 4))))
+
+    def test_heat_flow_matrix(self):
+        m = Material(thrm_cond_solids=1e-5)
+        for ip in self.thrm_e.int_pts:
+            ip.material = m
+            ip.void_ratio = 0.2
+            ip.deg_sat_water = 0.1
+        lam = self.thrm_e.int_pts[0].thrm_cond
+        jac = self.thrm_e.jacobian
+        expected = (1/40) * lam / jac * np.array(
+            [
+                [148, -189, 54, -13],
+                [-189, 432, -297, 54],
+                [54, -297, 432, -189],
+                [-13, 54, -189, 148],
+            ]
+        )
+        self.assertTrue(np.allclose(self.thrm_e.heat_flow_matrix, expected))
+
+    def test_heat_storage_matrix_uninitialized(self):
+        self.assertTrue(
+            np.allclose(self.thrm_e.heat_storage_matrix, np.zeros((4, 4)))
+        )
+
+    def test_heat_storage_matrix(self):
+        m = Material(spec_grav_solids=2.65, spec_heat_cap_solids=2.0e2)
+        for ip in self.thrm_e.int_pts:
+            ip.material = m
+            ip.void_ratio = 0.3
+            ip.deg_sat_water = 0.2
+        heat_cap = self.thrm_e.int_pts[0].vol_heat_cap
+        jac = self.thrm_e.jacobian
+        expected = (1/1680) * heat_cap * jac * np.array(
+            [
+                [128,   99,  -36,   19],
+                [99,  648,  -81,  -36],
+                [-36,  -81,  648,   99],
+                [19,  -36,   99,  128],
+            ]
+        )
+        self.assertTrue(np.allclose(
+            self.thrm_e.heat_storage_matrix, expected))
+
+
 class TestThermalBoundary1D(unittest.TestCase):
     def setUp(self):
         self.nodes = (Node1D(0, 2.0),)
-        self.bnd_el = Boundary1D(self.nodes)
+        self.int_pts = (IntegrationPoint1D(),)
+        self.bnd_el = Boundary1D(self.nodes, self.int_pts)
         self.thrm_bnd = ThermalBoundary1D(self.bnd_el)
 
     def test_invalid_no_parent(self):
@@ -91,7 +157,7 @@ class TestThermalBoundary1D(unittest.TestCase):
 
     def test_invalid_parent(self):
         with self.assertRaises(TypeError):
-            ThermalBoundary1D(self.nodes, order=1)
+            ThermalBoundary1D(self.nodes)
 
     def test_defaults(self):
         self.assertEqual(self.thrm_bnd.bnd_type,
@@ -101,6 +167,10 @@ class TestThermalBoundary1D(unittest.TestCase):
     def test_nodes_equal(self):
         for nd, bnd_nd in zip(self.nodes, self.thrm_bnd.nodes):
             self.assertIs(nd, bnd_nd)
+
+    def test_int_pts_equal(self):
+        for ip, bnd_ip in zip(self.int_pts, self.thrm_bnd.int_pts):
+            self.assertIs(ip, bnd_ip)
 
     def test_assign_bnd_type_invalid(self):
         with self.assertRaises(TypeError):
