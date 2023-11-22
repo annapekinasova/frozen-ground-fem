8a9148d32fcd37d4260d49627ced8179b828a498
Anna Pekinasova <anna.pekinasova@ucalgary.ca>
2023-11-21 22:01:46 -0700

    working progress, TBR

--- tests/test_consolidation.py
+++ tests/test_consolidation.py
@@ -7,6 +7,7 @@ from frozen_ground_fem.materials import (
 )
 from frozen_ground_fem.geometry import (
     Node1D,
+    IntegrationPoint1D,
     Element1D,
     Boundary1D,
 )
@@ -16,13 +17,11 @@ from frozen_ground_fem.consolidation import (
 )
 
 
-class TestConsolidationElement1D(unittest.TestCase):
+class TestConsolidationElement1DInvalid(unittest.TestCase):
     def setUp(self):
         self.nodes = tuple(Node1D(k, 2.0 * k + 1.0) for k in range(2))
-        self.e = Element1D(self.nodes, order=1)
-        self.thrm_e = ConsolidationElement1D(self.e)
 
-    def test_invalid_no_parent(self):
+    def test_no_parent(self):
         with self.assertRaises(TypeError):
             ConsolidationElement1D()
 
@@ -30,132 +29,226 @@ class TestConsolidationElement1D(unittest.TestCase):
         with self.assertRaises(TypeError):
             ConsolidationElement1D(self.nodes)
 
+
+class TestConsolidationElement1DLinear(unittest.TestCase):
+    def setUp(self):
+        self.nodes = tuple(Node1D(k, 2.0 * k + 1.0) for k in range(2))
+        self.e = Element1D(self.nodes, order=1)
+        self.consol_e = ConsolidationElement1D(self.e)
+
     def test_jacobian_equal(self):
-        self.assertAlmostEqual(self.e.jacobian, self.cond_e.jacobian)
+        self.assertAlmostEqual(self.e.jacobian, self.consol_e.jacobian)
 
     def test_nodes_equal(self):
-        for nd, e_nd in zip(self.nodes, self.cond_e.nodes):
+        for nd, e_nd in zip(self.nodes, self.consol_e.nodes):
             self.assertIs(nd, e_nd)
-            
-############# order=1 and order=3 tests
+
     def test_int_pts_equal(self):
-        for e_ip, te_ip in zip(self.e.int_pts, self.cond_e.int_pts):
+        for e_ip, te_ip in zip(self.e.int_pts, self.consol_e.int_pts):
             self.assertIs(e_ip, te_ip)
 
-    def test_stiffness_matrix_uninitialized_linear(self):
+    def test_stiffness_matrix_uninitialized(self):
         self.assertTrue(np.allclose(
-            self.cond_e.stiffness_matrix, np.zeros((2, 2))))
-            
-    def test_stiffness_matrix_uninitialized_cubic(self):
-        self.assertTrue(np.allclose(
-            self.cond_e.stiffness_matrix, np.zeros((4, 4))))
+            self.consol_e.stiffness_matrix, np.zeros((2, 2))))
 
     def test_stiffness_matrix(self):
-        m = Material(thrm_cond_solids=1e-5)
-        for ip in self.thrm_e.int_pts:
+        m = Material(spec_grav_solids=2.65, gam_w=9.801)
+        for ip in self.consol_e.int_pts:
             ip.material = m
-            ip.void_ratio = 0.2
+            ip.void_ratio_0 = 0.9
+            ip.void_ratio = 0.3
+            k = ip.hyd_cond
             ip.deg_sat_water = 0.1
-        lam = self.thrm_e.int_pts[0].thrm_cond
-        jac = self.thrm_e.jacobian
-        expected = lam / jac * np.array([[1.0, -1.0], [-1.0, 1.0]])
-        self.assertTrue(np.allclose(self.thrm_e.heat_flow_matrix, expected))
+            dsig_de = ip.eff_stress_gradient
+            dk_de = ip.hyd_cond_gradient
+            Gs = ip.material.spec_grav_solids
+        e_ratio = (1.0 + ip.void_ratio_0) / (1.0 + ip.void_ratio)
+        k_coef = dk_de * (Gs - 1.0) / (1.0 + e) - k * \
+                (Gs - 1.0) / (1.0 + e) ** 2
+        jac = self.consol_e.jacobian
+        expected = [k * e_ratio * dsig_de / gam_w /
+            jac * np.array([[1.0, -1.0], [-1.0, 1.0]])]
+           + [k_coef / jac * np.array([[1.0, -1.0], [-1.0, 1.0]])]
+        self.assertTrue(np.allclose(self.consol_e.stiffness_matrix, expected))
 
-    def test_heat_storage_matrix_uninitialized(self):
+    def test_mass_matrix_uninitialized(self):
         self.assertTrue(
-            np.allclose(self.thrm_e.heat_storage_matrix, np.zeros((2, 2)))
+            np.allclose(self.consol_e.mass_matrix, np.zeros((2, 2)))
         )
 
-    def test_heat_storage_matrix(self):
-        m = Material(spec_grav_solids=2.65, spec_heat_cap_solids=2.0e2)
-        for ip in self.thrm_e.int_pts:
+    def test_mass_matrix_matrix(self):
+        m = Material(spec_grav_ice=0.992)
+        for ip in self.consol.int_pts:
             ip.material = m
             ip.void_ratio = 0.3
+            ip.deg_sat_ice = 0.1
             ip.deg_sat_water = 0.2
-        heat_cap = self.thrm_e.int_pts[0].vol_heat_cap
-        jac = self.thrm_e.jacobian
-        expected = heat_cap * jac / 6 * np.array([[2.0, 1.0], [1.0, 2.0]])
+        jac = self.consol_e.jacobian
+        expected = jac * [(ip.deg_sat_water + spec_grav_ice * ip.deg_sat_ice)
+                          / (1.0 + ip.void_ratio_0)] * np.array([[2.0, 1.0], [1.0, 2.0]])
         self.assertTrue(np.allclose(
-            self.thrm_e.heat_storage_matrix, expected))
+            self.consol_e.mass_matrix_matrix, expected))
 
 
-class TestConsolidationBoundary1D(unittest.TestCase):
+class TestConsolidationElement1DCubic(unittest.TestCase):
     def setUp(self):
-        self.nodes = (Node1D(0, 2.0),)
-        self.bnd_el = Boundary1D(self.nodes)
-        self.thrm_bnd = ThermalBoundary1D(self.bnd_el)
+        self.nodes = tuple(Node1D(k, 2.0 * k + 1.0) for k in range(4))
+        self.e = Element1D(self.nodes, order=3)
+        self.consol_e = ConsolidationElement1D(self.e)
 
-    def test_invalid_no_parent(self):
-        with self.assertRaises(TypeError):
-            ThermalElement1D()
-
-    def test_invalid_parent(self):
-        with self.assertRaises(TypeError):
-            ThermalBoundary1D(self.nodes, order=1)
-
-    def test_defaults(self):
-        self.assertEqual(self.thrm_bnd.bnd_type,
-                         ThermalBoundary1D.BoundaryType.temp)
-        self.assertAlmostEqual(self.thrm_bnd.bnd_value, 0.0)
+    def test_jacobian_equal(self):
+        self.assertAlmostEqual(self.e.jacobian, self.consol_e.jacobian)
 
     def test_nodes_equal(self):
-        for nd, bnd_nd in zip(self.nodes, self.thrm_bnd.nodes):
-            self.assertIs(nd, bnd_nd)
+        for nd, e_nd in zip(self.nodes, self.consol_e.nodes):
+            self.assertIs(nd, e_nd)
 
-    def test_assign_bnd_type_invalid(self):
-        with self.assertRaises(TypeError):
-            self.thrm_bnd.bnd_type = 0
-        with self.assertRaises(TypeError):
-            self.thrm_bnd.bnd_type = "temp"
-        with self.assertRaises(AttributeError):
-            self.thrm_bnd.bnd_type = ThermalBoundary1D.BoundaryType.disp
+    def test_int_pts_equal(self):
+        for e_ip, te_ip in zip(self.e.int_pts, self.consol_e.int_pts):
+            self.assertIs(e_ip, te_ip)
 
-    def test_assign_bnd_type_valid(self):
-        self.thrm_bnd.bnd_type = ThermalBoundary1D.BoundaryType.heat_flux
-        self.assertEqual(
-            self.thrm_bnd.bnd_type, ThermalBoundary1D.BoundaryType.heat_flux
+    def test_stiffness_matrix_uninitialized(self):
+        self.assertTrue(np.allclose(
+            self.consol_e.stiffness_matrix, np.zeros((4, 4))))
+
+    def test_stiffness_matrix(self):
+        m = Material(spec_grav_solids=2.65, gam_w=9.801)
+        for ip in self.consol_e.int_pts:
+            ip.material = m
+            ip.void_ratio_0 = 0.9
+            ip.void_ratio = 0.3
+            k = ip.hyd_cond
+            ip.deg_sat_water = 0.1
+            dsig_de = ip.eff_stress_gradient
+            dk_de = ip.hyd_cond_gradient
+            Gs = ip.material.spec_grav_solids
+        e_ratio = (1.0 + ip.void_ratio_0) / (1.0 + ip.void_ratio)
+        k_coef = dk_de * (Gs - 1.0) / (1.0 + e) - k * \
+                (Gs - 1.0) / (1.0 + e) ** 2
+        jac = self.consol_e.jacobian
+        expected = (1/40) * [k * e_ratio * dsig_de / gam_w / jac * np.array([
+            [148, -189, 54, -13],
+                [-189, 432, -297, 54],
+                [54, -297, 432, -189],
+                [-13, 54, -189, 148],
+        ]])]
+            + (1/1680) * [k_coef / jac * np.array([[
+                [-840, 1197, -504, -147],
+                   [-1197, 0, 1701, -504],
+                   [504, -1701, 0, 1197],
+                   [-147, 504, -1197, 840],
+                ]])]
+
+            self.assertTrue(np.allclose(
+                self.consol_e.stiffness_matrix, expected))
+
+        def test_mass_matrix_uninitialized(self):
+           self.assertTrue(
+           np.allclose(self.consol_e.mass_matrix, np.zeros((4, 4)))
         )
-        self.thrm_bnd.bnd_type = ThermalBoundary1D.BoundaryType.temp
-        self.assertEqual(self.thrm_bnd.bnd_type,
-                         ThermalBoundary1D.BoundaryType.temp)
-        self.thrm_bnd.bnd_type = ThermalBoundary1D.BoundaryType.temp_grad
-        self.assertEqual(
-            self.thrm_bnd.bnd_type, ThermalBoundary1D.BoundaryType.temp_grad
+
+        def test_mass_matrix(self):
+            m = Material(spec_grav_ice=0.992)
+            for ip in self.consol_e.int_pts:
+                ip.material= m
+                ip.void_ratio = 0.3
+                ip.deg_sat_ice = 0.1
+                ip.deg_sat_water = 0.2
+            jac = self.consol_e.jacobian
+            expected = (1/1680) * jac * np.array(
+           [
+                [128,   99,  -36,   19],
+                [99,  648,  -81,  -36],
+                [-36,  -81,  648,   99],
+                [19,  -36,   99,  128],
+            ]
+        )
+            self.assertTrue(np.allclose(
+            self.consol_e.mass_matrix, expected))
+
+
+ class TestConsolidationBoundary1D(unittest.TestCase):
+    def setUp(self):
+        self.nodes = (Node1D(0, 2.0),)
+        self.int_pts = (IntegrationPoint1D(),)
+        self.bnd_el = Boundary1D(self.nodes, self.int_pts)
+        self.consol_bnd = ConsolidationBoundary1D(self.bnd_el)
+
+        def test_invalid_no_parent(self):
+        with self.assertRaises(TypeError):
+            ConsolidationElement1D()
+
+        def test_invalid_parent(self):
+        with self.assertRaises(TypeError):
+            ConsolidationBoundary1D(self.nodes)
+
+        def test_defaults(self):
+        self.assertEqual(self.consol_bnd.bnd_type,
+                        ConsolidationBoundary1D.BoundaryType.temp)
+                         self.assertAlmostEqual(self.consol_bnd.bnd_value, 0.0)
+
+                         def test_nodes_equal(self):
+                         for nd, bnd_nd in zip(self.nodes, self.consol_bnd.nodes):
+                         self.assertIs(nd, bnd_nd)
+
+                         def test_int_pts_equal(self):
+                         for ip, bnd_ip in zip(self.int_pts, self.consol_bnd.int_pts):
+                         self.assertIs(ip, bnd_ip)
+
+                         def test_assign_bnd_type_invalid(self):
+                         with self.assertRaises(TypeError):
+                         self.consol_bnd.bnd_type= 0
+                         with self.assertRaises(TypeError):
+                         self.consol_bnd.bnd_type= "temp"
+                         with self.assertRaises(AttributeError):
+                         self.consol_bnd.bnd_type= ConsolidationBoundary1D.BoundaryType.disp
+
+                         def test_assign_bnd_type_valid(self):
+                         self.consol_bnd.bnd_type= ConsolidationBoundary1D.BoundaryType.heat_flux
+                         self.assertEqual(
+            self.consol_bnd.bnd_type, ConsolidationBoundary1D.BoundaryType.heat_flux
+        )
+            self.consol_bnd.bnd_type = ConsolidationBoundary1D.BoundaryType.temp
+            self.assertEqual(self.consol_bnd.bnd_type,
+                        ConsolidationBoundary1D.BoundaryType.temp)
+                         self.consol_bnd.bnd_type = ConsolidationBoundary1D.BoundaryType.temp_grad
+                         self.assertEqual(
+            self.consol_bnd.bnd_type, ConsolidationBoundary1D.BoundaryType.temp_grad
         )
 
-    def test_assign_bnd_value_invalid(self):
+        def test_assign_bnd_value_invalid(self):
         with self.assertRaises(ValueError):
-            self.thrm_bnd.bnd_value = "temp"
+        self.consol_bnd.bnd_value = "temp"
 
-    def test_assign_bnd_value_valid(self):
-        self.thrm_bnd.bnd_value = 1.0
-        self.assertAlmostEqual(self.thrm_bnd.bnd_value, 1.0)
-        self.thrm_bnd.bnd_value = -2
-        self.assertAlmostEqual(self.thrm_bnd.bnd_value, -2.0)
-        self.thrm_bnd.bnd_value = "1e-5"
-        self.assertAlmostEqual(self.thrm_bnd.bnd_value, 1e-5)
+        def test_assign_bnd_value_valid(self):
+        self.consol_bnd.bnd_value= 1.0
+        self.assertAlmostEqual(self.consol_bnd.bnd_value, 1.0)
+        self.consol_bnd.bnd_value= -2
+        self.assertAlmostEqual(self.consol_bnd.bnd_value, -2.0)
+        self.consol_bnd.bnd_value= "1e-5"
+        self.assertAlmostEqual(self.consol_bnd.bnd_value, 1e-5)
 
-    def test_update_nodes_bnd_type_temp(self):
-        self.nodes[0].temp = 20.0
-        self.assertAlmostEqual(self.thrm_bnd.nodes[0].temp, 20.0)
-        self.thrm_bnd.bnd_type = ThermalBoundary1D.BoundaryType.temp
-        self.thrm_bnd.bnd_value = 5.0
-        self.assertAlmostEqual(self.thrm_bnd.nodes[0].temp, 20.0)
-        self.thrm_bnd.update_nodes()
-        self.assertAlmostEqual(self.thrm_bnd.nodes[0].temp, 5.0)
-        self.thrm_bnd.bnd_value = 7.0
-        self.assertAlmostEqual(self.thrm_bnd.nodes[0].temp, 5.0)
+        def test_update_nodes_bnd_type_temp(self):
+        self.nodes[0].temp= 20.0
+        self.assertAlmostEqual(self.consol_bnd.nodes[0].temp, 20.0)
+        self.consol_bnd.bnd_type= ConsolidationBoundary1D.BoundaryType.temp
+        self.consol_bnd.bnd_value= 5.0
+        self.assertAlmostEqual(self.consol_bnd.nodes[0].temp, 20.0)
+        self.consol_bnd.update_nodes()
+        self.assertAlmostEqual(self.consol_bnd.nodes[0].temp, 5.0)
+        self.consol_bnd.bnd_value= 7.0
+        self.assertAlmostEqual(self.consol_bnd.nodes[0].temp, 5.0)
 
-    def test_update_nodes_bnd_type_non_temp(self):
-        self.nodes[0].temp = 20.0
-        self.assertAlmostEqual(self.thrm_bnd.nodes[0].temp, 20.0)
-        self.thrm_bnd.bnd_type = ThermalBoundary1D.BoundaryType.heat_flux
-        self.thrm_bnd.bnd_value = 5.0
-        self.assertAlmostEqual(self.thrm_bnd.nodes[0].temp, 20.0)
-        self.thrm_bnd.update_nodes()
-        self.assertAlmostEqual(self.thrm_bnd.nodes[0].temp, 20.0)
-        self.thrm_bnd.bnd_type = ThermalBoundary1D.BoundaryType.temp_grad
-        self.thrm_bnd.bnd_value = 7.0
-        self.assertAlmostEqual(self.thrm_bnd.nodes[0].temp, 20.0)
-        self.thrm_bnd.update_nodes()
-        self.assertAlmostEqual(self.thrm_bnd.nodes[0].temp, 20.0)
+        def test_update_nodes_bnd_type_non_temp(self):
+        self.nodes[0].temp= 20.0
+        self.assertAlmostEqual(self.consol_bnd.nodes[0].temp, 20.0)
+        self.consol_bnd.bnd_type= ConsolidationBoundary1D.BoundaryType.heat_flux
+        self.consol_bnd.bnd_value= 5.0
+        self.assertAlmostEqual(self.consol_bnd.nodes[0].temp, 20.0)
+        self.consol_bnd.update_nodes()
+        self.assertAlmostEqual(self.consol_bnd.nodes[0].temp, 20.0)
+        self.consol_bnd.bnd_type= ConsolidationBoundary1D.BoundaryType.temp_grad
+        self.consol_bnd.bnd_value= 7.0
+        self.assertAlmostEqual(self.consol_bnd.nodes[0].temp, 20.0)
+        self.consol_bnd.update_nodes()
+        self.assertAlmostEqual(self.consol_bnd.nodes[0].temp, 20.0)
