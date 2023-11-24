d45b275143b5e390c7dfa1cdc9e992ec7813f00d
Brandon Karchewski <brandon.karchewski@ucalgary.ca>
2023-11-24 10:46:34 -0700

    code review with AP to complete consolidation tests

--- tests/test_consolidation.py
+++ tests/test_consolidation.py
@@ -72,6 +72,12 @@ class TestConsolidationElement1DLinear(unittest.TestCase):
             ip.void_ratio = 0.3
             ip.deg_sat_water = 0.1
             ip.pre_consol_stress = 1.0
+            k, dk_de = m.hyd_cond(ip.void_ratio, 1.0, False)
+            ip.hyd_cond = k
+            ip.hyd_cond_gradient = dk_de
+            sig, dsig_de = m.eff_stress(ip.void_ratio, ip.pre_consol_stress)
+            ip.eff_stress = sig
+            ip.eff_stress_gradient = dsig_de
         jac = self.consol_e.jacobian
         Gs = self.consol_e.int_pts[0].material.spec_grav_solids
         e0 = self.consol_e.int_pts[0].void_ratio_0
@@ -126,7 +132,6 @@ class TestConsolidationElement1DCubic(unittest.TestCase):
             self.assertIs(e_ip, te_ip)
 
     def test_stiffness_matrix_uninitialized(self):
-        # WHY THIS TEST IS DIFFERENT FOR MASS MATRIX? Line 178
         self.assertTrue(np.allclose(
             self.consol_e.stiffness_matrix, np.zeros((4, 4))))
 
@@ -149,6 +154,12 @@ class TestConsolidationElement1DCubic(unittest.TestCase):
             ip.void_ratio = 0.3
             ip.deg_sat_water = 0.1
             ip.pre_consol_stress = 1.0
+            k, dk_de = m.hyd_cond(ip.void_ratio, 1.0, False)
+            ip.hyd_cond = k
+            ip.hyd_cond_gradient = dk_de
+            sig, dsig_de = m.eff_stress(ip.void_ratio, ip.pre_consol_stress)
+            ip.eff_stress = sig
+            ip.eff_stress_gradient = dsig_de
         jac = self.consol_e.jacobian
         Gs = self.consol_e.int_pts[0].material.spec_grav_solids
         e0 = self.consol_e.int_pts[0].void_ratio_0
@@ -160,11 +171,11 @@ class TestConsolidationElement1DCubic(unittest.TestCase):
         coef_0 = k * e_ratio * dsig_de / gam_w / jac
         coef_1 = (dk_de * (Gs - 1.0) / (1.0 + e)
                   - k * (Gs - 1.0) / (1.0 + e) ** 2)
-        stiff_0 = coef_0 * 1/40 * np.array([[148, -189, 54, -13],
+        stiff_0 = coef_0 / 40.0 * np.array([[148, -189, 54, -13],
                                             [-189, 432, -297, 54],
                                             [54, -297, 432, -189],
                                             [-13, 54, -189, 148]])
-        stiff_1 = coef_1 * 1/1680 * np.array([[-840, 1197, -504, 147],
+        stiff_1 = coef_1 / 1680.0 * np.array([[-840, 1197, -504, 147],
                                               [-1197, 0, 1701, -504],
                                               [504, -1701, 0, 1197],
                                               [-147, 504, -1197, 840]])
@@ -172,16 +183,19 @@ class TestConsolidationElement1DCubic(unittest.TestCase):
         self.assertTrue(np.allclose(self.consol_e.stiffness_matrix, expected))
 
     def test_mass_matrix_uninitialized(self):
-        self.assertTrue(
-            np.allclose(self.consol_e.mass_matrix, np.zeros((4, 4)))
+        jac = self.consol_e.jacobian
+        expected = jac / 1680.0 * np.array(
+            [
+                [128,   99,  -36,   19],
+                [99,  648,  -81,  -36],
+                [-36,  -81,  648,   99],
+                [19,  -36,   99,  128],
+            ]
         )
-#   def test_mass_matrix_uninitialized(self):
-#        jac = self.consol_e.jacobian
-#        ????expected = jac / 6.0 * np.array([[2.0, 1.0], [1.0, 2.0]])
-#        self.assertTrue(
-#            np.allclose(self.consol_e.mass_matrix,
-#                        expected)
-#       )
+        self.assertTrue(
+            np.allclose(self.consol_e.mass_matrix,
+                        expected)
+        )
 
     def test_mass_matrix(self):
         for ip in self.consol_e.int_pts:
@@ -191,23 +205,23 @@ class TestConsolidationElement1DCubic(unittest.TestCase):
         e0 = self.consol_e.int_pts[0].void_ratio_0
         Sw = self.consol_e.int_pts[0].deg_sat_water
         coef = (Sw + Gi * (1.0 - Sw)) / (1.0 + e0)
-        expected = 1/1680 * coef * jac * / 6.0 * (
-            np.array([
+        expected = coef * jac / 1680.0 * np.array(
+            [
                 [128,   99,  -36,   19],
                 [99,  648,  -81,  -36],
                 [-36,  -81,  648,   99],
-                [19,  -36,   99,  128]
-            )
-          )
+                [19,  -36,   99,  128],
+            ]
+        )
         self.assertTrue(np.allclose(self.consol_e.mass_matrix, expected))
 
 
 class TestConsolidationBoundary1D(unittest.TestCase):
     def setUp(self):
-        self.nodes=(Node1D(0, 2.0),)
-        self.int_pts=(IntegrationPoint1D(),)
-        self.bnd_el=Boundary1D(self.nodes, self.int_pts)
-        self.consol_bnd=ConsolidationBoundary1D(self.bnd_el)
+        self.nodes = (Node1D(0, 2.0),)
+        self.int_pts = (IntegrationPoint1D(),)
+        self.bnd_el = Boundary1D(self.nodes, self.int_pts)
+        self.consol_bnd = ConsolidationBoundary1D(self.bnd_el)
 
     def test_invalid_no_parent(self):
         with self.assertRaises(TypeError):
@@ -223,25 +237,25 @@ class TestConsolidationBoundary1D(unittest.TestCase):
         self.assertAlmostEqual(self.consol_bnd.bnd_value, 0.0)
 
     def test_assign_bnd_type_invalid(self):
-        cb1d=ConsolidationBoundary1D
+        cb1d = ConsolidationBoundary1D
         with self.assertRaises(TypeError):
-            self.consol_bnd.bnd_type=0
+            self.consol_bnd.bnd_type = 0
         with self.assertRaises(TypeError):
-            self.consol_bnd.bnd_type="temp"
+            self.consol_bnd.bnd_type = "temp"
         with self.assertRaises(AttributeError):
-            self.consol_bnd.bnd_type=cb1d.BoundaryType.disp
+            self.consol_bnd.bnd_type = cb1d.BoundaryType.disp
 
     def test_assign_bnd_type_valid(self):
-        cb1d=ConsolidationBoundary1D
-        self.consol_bnd.bnd_type=cb1d.BoundaryType.void_ratio
+        cb1d = ConsolidationBoundary1D
+        self.consol_bnd.bnd_type = cb1d.BoundaryType.void_ratio
         self.assertEqual(
             self.consol_bnd.bnd_type,
             ConsolidationBoundary1D.BoundaryType.void_ratio
         )
-        self.consol_bnd.bnd_type=cb1d.BoundaryType.fixed_flux
+        self.consol_bnd.bnd_type = cb1d.BoundaryType.fixed_flux
         self.assertEqual(self.consol_bnd.bnd_type,
                          ConsolidationBoundary1D.BoundaryType.fixed_flux)
-        self.consol_bnd.bnd_type=cb1d.BoundaryType.water_flux
+        self.consol_bnd.bnd_type = cb1d.BoundaryType.water_flux
         self.assertEqual(
             self.consol_bnd.bnd_type,
             ConsolidationBoundary1D.BoundaryType.water_flux
@@ -249,39 +263,39 @@ class TestConsolidationBoundary1D(unittest.TestCase):
 
     def test_assign_bnd_value_invalid(self):
         with self.assertRaises(ValueError):
-            self.consol_bnd.bnd_value="temp"
+            self.consol_bnd.bnd_value = "temp"
 
     def test_assign_bnd_value_valid(self):
-        self.consol_bnd.bnd_value=1.0
+        self.consol_bnd.bnd_value = 1.0
         self.assertAlmostEqual(self.consol_bnd.bnd_value, 1.0)
-        self.consol_bnd.bnd_value=-2
+        self.consol_bnd.bnd_value = -2
         self.assertAlmostEqual(self.consol_bnd.bnd_value, -2.0)
-        self.consol_bnd.bnd_value="1e-5"
+        self.consol_bnd.bnd_value = "1e-5"
         self.assertAlmostEqual(self.consol_bnd.bnd_value, 1e-5)
 
     def test_update_nodes_bnd_type_void_ratio(self):
-        cb1d=ConsolidationBoundary1D
-        self.nodes[0].void_ratio=0.6
+        cb1d = ConsolidationBoundary1D
+        self.nodes[0].void_ratio = 0.6
         self.assertAlmostEqual(self.consol_bnd.nodes[0].void_ratio, 0.6)
-        self.consol_bnd.bnd_type=cb1d.BoundaryType.void_ratio
-        self.consol_bnd.bnd_value=0.3
+        self.consol_bnd.bnd_type = cb1d.BoundaryType.void_ratio
+        self.consol_bnd.bnd_value = 0.3
         self.assertAlmostEqual(self.consol_bnd.nodes[0].void_ratio, 0.6)
         self.consol_bnd.update_nodes()
         self.assertAlmostEqual(self.consol_bnd.nodes[0].void_ratio, 0.3)
-        self.consol_bnd.bnd_value=0.4
+        self.consol_bnd.bnd_value = 0.4
         self.assertAlmostEqual(self.consol_bnd.nodes[0].void_ratio, 0.3)
 
     def test_update_nodes_bnd_type_non_void_ratio(self):
-        cb1d=ConsolidationBoundary1D
-        self.nodes[0].void_ratio=0.6
+        cb1d = ConsolidationBoundary1D
+        self.nodes[0].void_ratio = 0.6
         self.assertAlmostEqual(self.consol_bnd.nodes[0].void_ratio, 0.6)
-        self.consol_bnd.bnd_type=cb1d.BoundaryType.fixed_flux
-        self.consol_bnd.bnd_value=5.0
+        self.consol_bnd.bnd_type = cb1d.BoundaryType.fixed_flux
+        self.consol_bnd.bnd_value = 5.0
         self.assertAlmostEqual(self.consol_bnd.nodes[0].void_ratio, 0.6)
         self.consol_bnd.update_nodes()
         self.assertAlmostEqual(self.consol_bnd.nodes[0].void_ratio, 0.6)
-        self.consol_bnd.bnd_type=cb1d.BoundaryType.water_flux
-        self.consol_bnd.bnd_value=7.0
+        self.consol_bnd.bnd_type = cb1d.BoundaryType.water_flux
+        self.consol_bnd.bnd_value = 7.0
         self.assertAlmostEqual(self.consol_bnd.nodes[0].void_ratio, 0.6)
         self.consol_bnd.update_nodes()
         self.assertAlmostEqual(self.consol_bnd.nodes[0].void_ratio, 0.6)
