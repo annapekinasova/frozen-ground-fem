4cc79631720d0a821a58dadf6997f81ac2e31fb8
Brandon Karchewski <brandon.karchewski@ucalgary.ca>
2023-12-21 09:23:07 -0700

    added invalid Element1D.order tests, refactored invalid tests

--- tests/test_geometry_Element1D.py
+++ tests/test_geometry_Element1D.py
@@ -8,37 +8,63 @@ from frozen_ground_fem.geometry import (
 )
 
 
+class TestElement1DInvalid(unittest.TestCase):
+    def test_initialize_without_nodes(self):
+        with self.assertRaises(TypeError):
+            Element1D()
+
+    def test_initialize_too_few_nodes_linear(self):
+        with self.assertRaises(ValueError):
+            nodes = (Node1D(0),)
+            Element1D(nodes, order=1)
+
+    def test_initialize_too_many_nodes_linear(self):
+        with self.assertRaises(ValueError):
+            nodes = tuple(Node1D(k, 2.0 * k + 1.0) for k in range(3))
+            Element1D(nodes, order=1)
+
+    def test_initialize_too_few_nodes_cubic(self):
+        with self.assertRaises(ValueError):
+            nodes = tuple(Node1D(k, 2.0 * k + 1.0) for k in range(3))
+            Element1D(nodes, order=3)
+
+    def test_initialize_too_many_nodes_cubic(self):
+        with self.assertRaises(ValueError):
+            nodes = tuple(Node1D(k, 2.0 * k + 1.0) for k in range(5))
+            Element1D(nodes, order=3)
+
+    def test_initialize_invalid_nodes_linear(self):
+        with self.assertRaises(TypeError):
+            nodes = tuple(k for k in range(2))
+            Element1D(nodes, order=1)
+
+    def test_initialize_invalid_nodes_cubic(self):
+        with self.assertRaises(TypeError):
+            nodes = tuple(k for k in range(4))
+            Element1D(nodes, order=3)
+
+    def test_initialize_invalid_order(self):
+        with self.assertRaises(ValueError):
+            nodes = tuple(Node1D(k, 2.0 * k + 1.0) for k in range(3))
+            Element1D(nodes, order=2)
+
+    def test_set_invalid_int_pt(self):
+        with self.assertRaises(AttributeError):
+            nodes = tuple(Node1D(k, 2.0 * k + 1.0) for k in range(4))
+            e = Element1D(nodes)
+            e.int_pts = 3
+
+
 class TestElement1DLinear(unittest.TestCase):
     def setUp(self):
         self.nodes = tuple(Node1D(k, 2.0 * k + 1.0) for k in range(2))
         self.e = Element1D(self.nodes, order=1)
 
-    def test_initialize_without_nodes(self):
-        with self.assertRaises(TypeError):
-            Element1D()
-
     def test_initialize_valid_nodes_value(self):
         self.assertEqual(self.e.nodes[1].z, 3.0)
 
     def test_initialize_valid_nodes_type(self):
-        nodes = list(self.nodes)
-        e = Element1D(nodes, order=1)
-        self.assertIsInstance(e.nodes, tuple)
-
-    def test_initialize_too_few_nodes(self):
-        with self.assertRaises(ValueError):
-            nodes = (Node1D(0),)
-            Element1D(nodes, order=1)
-
-    def test_initialize_too_many_nodes(self):
-        with self.assertRaises(ValueError):
-            nodes = tuple(Node1D(k, 2.0 * k + 1.0) for k in range(3))
-            Element1D(nodes, order=1)
-
-    def test_initialize_invalid_nodes(self):
-        with self.assertRaises(TypeError):
-            nodes = tuple(k for k in range(2))
-            Element1D(nodes, order=1)
+        self.assertIsInstance(self.e.nodes, tuple)
 
     def test_jacobian_value(self):
         self.assertEqual(self.e.jacobian, 2.0)
@@ -64,10 +90,6 @@ class TestElement1DLinear(unittest.TestCase):
 
     def test_int_pt_type(self):
         self.assertIsInstance(self.e.int_pts, tuple)
-
-    def test_set_int_pt(self):
-        with self.assertRaises(AttributeError):
-            self.e.int_pts = 3
 
 # TODO: add tests for cubic case
 
