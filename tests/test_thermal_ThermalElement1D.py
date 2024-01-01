41b26261ed3b6a89c9572bbbef2418a0fe63a050
Brandon Karchewski <brandon.karchewski@ucalgary.ca>
2024-01-01 12:00:45 -0700

    added tests for temp_rate, temp_gradient, and water_flux_rate

--- tests/test_thermal_ThermalElement1D.py
+++ tests/test_thermal_ThermalElement1D.py
@@ -84,32 +84,55 @@ class TestThermalElement1DLinear(unittest.TestCase):
     def test_update_integration_points_null_material(self):
         self.thrm_e.nodes[0].temp = -1.0
         self.thrm_e.nodes[1].temp = +2.0
+        self.thrm_e.nodes[0].temp_rate = 0.2
+        self.thrm_e.nodes[1].temp_rate = 0.4
         self.thrm_e.update_integration_points()
         self.assertAlmostEqual(self.thrm_e.int_pts[0].temp, -0.366025403784439)
         self.assertAlmostEqual(self.thrm_e.int_pts[1].temp, 1.366025403784440)
+        self.assertAlmostEqual(
+            self.thrm_e.int_pts[0].temp_rate, 0.242264973081037)
+        self.assertAlmostEqual(
+            self.thrm_e.int_pts[1].temp_rate, 0.357735026918963)
+        self.assertAlmostEqual(self.thrm_e.int_pts[0].temp_gradient, 1.5)
+        self.assertAlmostEqual(self.thrm_e.int_pts[1].temp_gradient, 1.5)
         self.assertAlmostEqual(self.thrm_e.int_pts[0].deg_sat_water, 0.0)
         self.assertAlmostEqual(self.thrm_e.int_pts[1].deg_sat_water, 1.0)
         self.assertAlmostEqual(
             self.thrm_e.int_pts[0].deg_sat_water_temp_gradient, 0.0)
         self.assertAlmostEqual(
             self.thrm_e.int_pts[1].deg_sat_water_temp_gradient, 0.0)
-        # TODO: test temp_rate, temp_gradient, water_flux_rate
+        self.assertAlmostEqual(
+            self.thrm_e.int_pts[0].water_flux_rate, 0.0
+        )
+        self.assertAlmostEqual(
+            self.thrm_e.int_pts[1].water_flux_rate, 0.0
+        )
 
     def test_update_integration_points_with_material(self):
-        m = Material(deg_sat_water_alpha=1.2e4, deg_sat_water_beta=0.35)
+        m = Material(
+            deg_sat_water_alpha=1.2e4,
+            deg_sat_water_beta=0.35,
+            water_flux_b1=0.08,
+            water_flux_b2=4.0,
+            water_flux_b3=10.0e-6,
+            seg_pot_0=2e-9,
+        )
         for ip in self.thrm_e.int_pts:
             ip.material = m
+            ip.tot_stress = 120.0e3
         self.thrm_e.nodes[0].temp = -1.0
         self.thrm_e.nodes[1].temp = +2.0
+        self.thrm_e.nodes[0].temp_rate = 0.2
+        self.thrm_e.nodes[1].temp_rate = 0.4
         self.thrm_e.update_integration_points()
+        self.assertAlmostEqual(self.thrm_e.int_pts[0].temp, -0.366025403784439)
+        self.assertAlmostEqual(self.thrm_e.int_pts[1].temp, 1.366025403784440)
         self.assertAlmostEqual(
-            self.thrm_e.int_pts[0].temp,
-            -0.366025403784439,
-        )
+            self.thrm_e.int_pts[0].temp_rate, 0.242264973081037)
         self.assertAlmostEqual(
-            self.thrm_e.int_pts[1].temp,
-            1.366025403784440,
-        )
+            self.thrm_e.int_pts[1].temp_rate, 0.357735026918963)
+        self.assertAlmostEqual(self.thrm_e.int_pts[0].temp_gradient, 1.5)
+        self.assertAlmostEqual(self.thrm_e.int_pts[1].temp_gradient, 1.5)
         self.assertAlmostEqual(
             self.thrm_e.int_pts[0].deg_sat_water,
             0.149711781050801,
@@ -126,7 +149,15 @@ class TestThermalElement1DLinear(unittest.TestCase):
             self.thrm_e.int_pts[1].deg_sat_water_temp_gradient,
             0.0,
         )
-        # TODO: test temp_rate, temp_gradient, water_flux_rate
+        self.assertAlmostEqual(
+            self.thrm_e.int_pts[0].water_flux_rate,
+            1.1378090109e-10,
+            delta=1e-17,
+        )
+        self.assertAlmostEqual(
+            self.thrm_e.int_pts[1].water_flux_rate, 0.0,
+            delta=1e-17,
+        )
 
 
 class TestThermalElement1DCubic(unittest.TestCase):
@@ -227,8 +258,15 @@ class TestThermalElement1DCubic(unittest.TestCase):
             1.10,
             2.00,
         ])
-        for T, nd in zip(Te, self.thrm_e.nodes):
+        dTdte = np.array([
+            -0.5,
+            -0.1,
+            0.5,
+            0.8,
+        ])
+        for T, dTdt, nd in zip(Te, dTdte, self.thrm_e.nodes):
             nd.temp = T
+            nd.temp_rate = dTdt
         self.thrm_e.update_integration_points()
         expected_Tip = np.array([
             -0.913964840018686,
@@ -236,6 +274,20 @@ class TestThermalElement1DCubic(unittest.TestCase):
             0.500000000000000,
             1.436743906025890,
             1.913964840018690,
+        ])
+        expected_dTdtip = np.array([
+            -0.474536483402387,
+            -0.267597978008154,
+            0.206250000000000,
+            0.647478693241513,
+            0.794655768169027,
+        ])
+        expected_dTdZip = np.array([
+            0.33535785429992,
+            0.51464214570008,
+            0.61250000000000,
+            0.51464214570008,
+            0.33535785429992,
         ])
         expected_Sw = np.array([
             0.0,
@@ -251,29 +303,56 @@ class TestThermalElement1DCubic(unittest.TestCase):
             0.0,
             0.0,
         ])
-        for ip, eT, eSw, edSw in zip(
+        expected_qw = np.array([
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+        ])
+        for ip, eT, edTdt, edTdZ, eSw, edSw, eqw in zip(
             self.thrm_e.int_pts,
             expected_Tip,
+            expected_dTdtip,
+            expected_dTdZip,
             expected_Sw,
             expected_dSw_dT,
+            expected_qw,
         ):
             self.assertAlmostEqual(ip.temp, eT)
+            self.assertAlmostEqual(ip.temp_rate, edTdt)
+            self.assertAlmostEqual(ip.temp_gradient, edTdZ)
             self.assertAlmostEqual(ip.deg_sat_water, eSw)
             self.assertAlmostEqual(ip.deg_sat_water_temp_gradient, edSw)
-            # TODO: test temp_rate, temp_gradient, water_flux_rate
+            self.assertAlmostEqual(ip.water_flux_rate, eqw)
 
     def test_update_integration_points_with_material(self):
-        m = Material(deg_sat_water_alpha=1.2e4, deg_sat_water_beta=0.35)
+        m = Material(
+            deg_sat_water_alpha=1.2e4,
+            deg_sat_water_beta=0.35,
+            water_flux_b1=0.08,
+            water_flux_b2=4.0,
+            water_flux_b3=10.0e-6,
+            seg_pot_0=2e-9,
+        )
         for ip in self.thrm_e.int_pts:
             ip.material = m
+            ip.tot_stress = 120.0e3
         Te = np.array([
             -1.00,
             -0.10,
             1.10,
             2.00,
         ])
-        for T, nd in zip(Te, self.thrm_e.nodes):
+        dTdte = np.array([
+            -0.5,
+            -0.1,
+            0.5,
+            0.8,
+        ])
+        for T, dTdt, nd in zip(Te, dTdte, self.thrm_e.nodes):
             nd.temp = T
+            nd.temp_rate = dTdt
         self.thrm_e.update_integration_points()
         expected_Tip = np.array([
             -0.913964840018686,
@@ -281,6 +360,20 @@ class TestThermalElement1DCubic(unittest.TestCase):
             0.500000000000000,
             1.436743906025890,
             1.913964840018690,
+        ])
+        expected_dTdtip = np.array([
+            -0.474536483402387,
+            -0.267597978008154,
+            0.206250000000000,
+            0.647478693241513,
+            0.794655768169027,
+        ])
+        expected_dTdZip = np.array([
+            0.33535785429992,
+            0.51464214570008,
+            0.61250000000000,
+            0.51464214570008,
+            0.33535785429992,
         ])
         expected_Sw = np.array([
             0.0915235681884727,
@@ -296,16 +389,28 @@ class TestThermalElement1DCubic(unittest.TestCase):
             0.00000000000000,
             0.00000000000000,
         ])
-        for ip, eT, eSw, edSw in zip(
+        expected_qw = np.array([
+            -1.3562595181e-11,
+            -1.3792048602e-10,
+            0.0,
+            0.0,
+            0.0,
+        ])
+        for ip, eT, edTdt, edTdZ, eSw, edSw, eqw in zip(
             self.thrm_e.int_pts,
             expected_Tip,
+            expected_dTdtip,
+            expected_dTdZip,
             expected_Sw,
             expected_dSw_dT,
+            expected_qw,
         ):
             self.assertAlmostEqual(ip.temp, eT)
+            self.assertAlmostEqual(ip.temp_rate, edTdt)
+            self.assertAlmostEqual(ip.temp_gradient, edTdZ)
             self.assertAlmostEqual(ip.deg_sat_water, eSw)
             self.assertAlmostEqual(ip.deg_sat_water_temp_gradient, edSw)
-            # TODO: test temp_rate, temp_gradient, water_flux_rate
+            self.assertAlmostEqual(ip.water_flux_rate, eqw, delta=1e-18)
 
 
 if __name__ == "__main__":
