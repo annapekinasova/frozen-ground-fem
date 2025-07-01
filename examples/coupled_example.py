c92a73f720e348d5f39760a4836d08b03b7b4199
Brandon Karchewski <brandon.karchewski@ucalgary.ca>
2025-07-01 11:07:11 -0600

    fixed this simple example of element object creation

--- examples/coupled_example.py
+++ examples/coupled_example.py
@@ -9,14 +9,19 @@ from frozen_ground_fem.coupled import (
 
 def main():
     e = Element1D([Node1D(k, 2 * k) for k in range(4)])
-    print(f"Element1D, id={id(e)}")
+    print(">>> e = Element1D([Node1D(k, 2 * k) for k in range(4)])")
+    print(f"e={e}")
+    print(f"id(e)={id(e)}")
+    print(f"id(e.nodes)={id(e.nodes)}")
     for nd in e.nodes:
         print(f"index={nd.index}, z={nd.z}, id={id(nd)}")
     print()
 
-    ce = CoupledElement1D(e)
-    print(f"CoupledElement1D, id={id(ce)}")
-    print(f"parent id={id(ce._parent)}")
+    ce = CoupledElement1D(e.nodes)
+    print(">>> ce = CoupledElement1D(e.nodes)")
+    print(f"ce={ce}")
+    print(f"id(ce)={id(ce)}")
+    print(f"id(ce.nodes)={id(ce.nodes)}")
     for nd in ce.nodes:
         print(f"index={nd.index}, z={nd.z}, id={id(nd)}")
 
