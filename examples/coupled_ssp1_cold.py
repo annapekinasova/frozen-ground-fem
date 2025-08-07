9a2a761c7d8400b82db24e7c0fcd0d0299b27051
Brandon Karchewski <brandon.karchewski@ucalgary.ca>
2025-08-07 13:44:28 -0600

    updated tolerance and to bi-weekly data for cold ssp1

--- examples/coupled_ssp1_cold.py
+++ examples/coupled_ssp1_cold.py
@@ -41,7 +41,7 @@ def main():
         nd.z = zn
 
     # define plotting time increments
-    t_plot_targ = np.linspace(0.0, 1356.0, 1357) * s_per_wk
+    t_plot_targ = np.linspace(0.0, 1356.0, 679) * s_per_wk
 
     # define analysis parameters
     dt_sim_0 = 0.05 * s_per_day
@@ -51,6 +51,7 @@ def main():
     tol_str = f"{tol:0.1e}"
     tol_str = "p".join(tol_str.split("."))
     fname = "examples/" + f"coupled_ssp1_sat_cold_{ta.num_elements}_{tol_str}_"
+    tol = 1e-3
     print(fname)
 
     # define material properties
